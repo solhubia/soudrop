ba26e7db89824551265d205ffbb45fb4fde26a35
Brenodev <brenodstk@gmail.com>
2025-10-07 00:27:57 -0300

    teste

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
