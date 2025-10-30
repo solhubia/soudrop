42b0776e83ca86b08e31223126b41740b646a4b6
Brenodev <brenodstk@gmail.com>
2025-10-29 22:53:06 -0300

    arrumando PageView

--- src/lib/fbq.ts
+++ src/lib/fbq.ts
@@ -1,21 +1,6 @@
 // src/lib/fbq.ts
 declare global { interface Window { fbq: any } }
 
-export function initPixel(pixelId: string) {
-  if (window.fbq) return;
-  !(function(f,b,e,v,n,t,s){
-    if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode!.insertBefore(t,s);
-  })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-
-  window.fbq("init", pixelId);
-  window.fbq("track", "PageView");
-}
-
 // FUNÇÕES QUE ENVIAM EVENTOS COM O PARÂMETRO content_category
 export function trackViewContent(category: "club" | "soudrop" | "courses", extra?: {
   content_name?: string, content_ids?: string[], value?: number, currency?: string
